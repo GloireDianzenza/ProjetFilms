fc4cbdd813b1edee8b8726c1e44fc442ad0e46fd
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-24 11:10:48 +0200

    js11

--- js/achat.js
+++ js/achat.js
@@ -3,3 +3,13 @@ import {clamp,clampOperation,isInteger,resetInputNum} from "./common.mjs";
 setInterval(()=>{
     resetInputNum(numProducts,1);
 },100);
+
+$('.range-minus').click(function(){
+    if(!isInteger(numProducts.value))
+    {
+        resetInputNum(numProducts,1);
+    }
+    else{
+
+    }
+});
