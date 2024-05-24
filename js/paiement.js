8cb74ca85714a0533d0509d5f94440efc2599b29
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-24 12:04:45 +0200

    paiement5

--- js/paiement.js
+++ js/paiement.js
@@ -1,2 +1,11 @@
 import {clamp,clampOperation,isInteger,resetInputNum} from "./common.mjs";
 
+setInterval(()=>{
+    var index = 0;
+    while(index < cardNum.value.length){
+        if(true){
+
+        }
+        index++;
+    }
+},100);
