e979f8957fabf10c7607db8bb43185de6e006d1a
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-27 12:32:10 +0200

    search9

--- js/search.js
+++ js/search.js
@@ -29,3 +29,64 @@ const movieList = [
         favorite:true
     }
 ]
+
+function search(sort="recents",filter="nom",input=""){
+    document.querySelector(".movies").innerHTML = "";
+    let acceptedMovies = [];
+    if(input.trim() == "")
+    {
+        for(var movie of movieList)
+        {
+            acceptedMovies.push(movie);
+        }
+    }
+    else{
+
+    }
+
+        for(let movie of acceptedMovies)
+        {
+            var movieDiv = document.createElement("div");
+            movieDiv.classList.add("movie");
+            movieDiv.innerHTML += `<img src="${movie.image}">`;
+            movieDiv.innerHTML += `<h3>${movie.title}</h3>`;
+            movieDiv.innerHTML += `<p>${movie.year}</p>`;
+            movieDiv.innerHTML += `<p>${movie.director}</p>`;
+            movieDiv.innerHTML += `<p>${movie.plot}</p>`;
+            movieDiv.innerHTML += `<p>${movie.price} €</p>`;
+            var starsDiv = document.createElement("div");
+            starsDiv.classList.add("stars");
+            var a = 0;
+            for(var i = 0;i < movie.note;i++){
+                var starImg = document.createElement("img");
+                starImg.src = "../svg/yellow-star-15589.svg";
+                starsDiv.appendChild(starImg);
+                a++;
+            }
+            while(a < 5){
+                var starImg = document.createElement("img");
+                starImg.src = "../svg/black-star-icon.svg";
+                starsDiv.appendChild(starImg);
+                a++;
+            }
+            console.log(a);
+            movieDiv.appendChild(starsDiv);
+
+            var heartImg = document.createElement("img");
+            heartImg.classList.add("favorite");
+            if(movie.favorite)
+            {
+                heartImg.src = "../svg/red-heart-icon.svg";
+            }
+            else
+            {
+                heartImg.src = "../svg/black-heart-svgrepo-com.svg";
+            }
+            movieDiv.appendChild(heartImg);
+            movieDiv.innerHTML += `<a href="achat.html?namr=toto"><button>Acheter</button></a>
+            <button>Ajouter au panier</button>`;
+
+            document.querySelector(".movies").appendChild(movieDiv);
+        }
+}
+search()
