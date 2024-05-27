d9f57d38bee02c4cab76191f17368fa7f245bd57
GloireDianzenza <gloiredianzenza5@gmail.com>
2024-05-27 11:55:03 +0200

    search8

--- js/search.js
+++ js/search.js
@@ -12,5 +12,20 @@ $('#sortBtn li').click(function(){
 $("#filterBtn li").click(function(){
     var value = $(this).attr('value');
     var text = $(this).html();
-    console.log(text);
+    $('#filterBtn').attr("value",value);
+    $('#filterBtn > p').html(text);
 })
+
+
+const movieList = [
+    {
+        image:"../img/Titanic (James Cameron, 1997)_0000.jpg",
+        title:"Titanic",
+        year:1997,
+        director:"James Cameron",
+        plot:"The supposedly unsinkable RMS Titanic, carrying dreams and heartbreak like Jack and Rose, met a tragic end after striking an iceberg on its maiden voyage.",
+        price:15,
+        note:5,
+        favorite:true
+    }
+]
